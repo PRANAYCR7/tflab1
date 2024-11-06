2722d51debab7719e8e60bfb578444f0e1510848
PRANAYCR7 <91401075+PRANAYCR7@users.noreply.github.com>
2024-11-06 07:55:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,14 +25,14 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.pranayvnet}"
+  name                = "${var.vnet}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   address_space       = ["10.10.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "${var.pranaysubnet}"
+  name                 = "${var.subnet}"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.10.1.0/24"]
