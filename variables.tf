3e6c9ed4507573ffd8ffc128d87c4afa560236fa
PRANAYCR7 <91401075+PRANAYCR7@users.noreply.github.com>
2024-11-06 07:37:17 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "rgname" {
+    type = string
+    description = "used for naming the resource group"
+}
+
+variable "location" {
+    type = string
+    description = "used to select the location"
+}
