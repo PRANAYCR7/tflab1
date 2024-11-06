b31fd848f0f18318eb1ccba64da1eef5fd99af9d
PRANAYCR7 <91401075+PRANAYCR7@users.noreply.github.com>
2024-11-06 20:00:53 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,9 @@
-
+variable "rgname" {
+   type = string
+   description = "this is to name the resource group"
+}
+variable "location" {
+   type = string
+   description = "this is to name the location"
+   default = "australiaeast"
+}
